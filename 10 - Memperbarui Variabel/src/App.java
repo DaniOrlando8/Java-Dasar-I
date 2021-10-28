029bc2b1cec90cbb3849603793505ae5fbcd1377
Dani Orlando <daniorlando8@gmail.com>
2021-10-28 16:38:03 +0700

    30/10/2021

--- 10 - Memperbarui Variabel/src/App.java
+++ 10 - Memperbarui Variabel/src/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) {
-        
+        //Inisialisasi Variabel
+        String name = "Bambang";
+        System.out.println(name);
+
+        //Memperbaharui Variabel
+        name = "Parjo";
+        System.out.println(name);
     }
 }
