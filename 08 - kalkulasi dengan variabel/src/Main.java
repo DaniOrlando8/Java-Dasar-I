2e58871832d1b6d9b07376c2837dde066eadb5c4
Dani Orlando <daniorlando8@gmail.com>
2021-10-28 20:59:45 +0700

    28/10/2021

--- 08 - kalkulasi dengan variabel/src/Main.java
+++ 08 - kalkulasi dengan variabel/src/Main.java
@@ -3,7 +3,7 @@
     Jika variable adalah sebuah integer, Anda dapat melakukan kalkulasi dengannya,
     sama seperti dengan nilai integer biasa.
     Anda dapat menggunakannya dengan nilai integer lainnya, atau variable integer.
-*/
+*/ 
 public class Main {
     public static void main(String[] args) {
         int number1 = 10;
