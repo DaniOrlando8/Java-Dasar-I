ce2da519b374b3d368cae4ef17637e2a05711254
Dani Orlando <daniorlando8@gmail.com>
2021-10-28 16:50:08 +0700

    30/10/2021

---
+++ 10 - Memperbarui Variabel/src/Soal.java
@@ -0,0 +1,21 @@
+public class Soal {
+    public static void main(String[] args) {
+        int number = 8;
+        String text = "JavaScript";
+        System.out.println(number);
+        System.out.println(text);
+        
+        // Perbarui variable number dengan 9
+        
+        
+        // Cetak variable number
+        
+        
+        // Perbarui variable text dengan "Java"
+        
+        
+        // Cetak variable text
+        
+            
+    }
+}  
