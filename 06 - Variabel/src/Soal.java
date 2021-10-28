de3f4e113b2bd74855797c33f4c4cd8dd6228bae
Dani Orlando <daniorlando8@gmail.com>
2021-10-28 15:00:24 +0700

    30/10/2020

---
+++ 06 - Variabel/src/Soal.java
@@ -0,0 +1,25 @@
+/* ~ Soal ~
+    1. Kerjakanlah soal dibawah ini.
+*/
+public class Soal {
+    public static void main(String[] args) {
+/*
+    Deklarasikan variabel nomor dengan tipe data int 
+
+
+    Tetapkan 3 ke variabel nomor
+
+
+    Cetak variabel nomor
+
+//---------------------------------------------------//
+    Deklarasikan variabel nomor dengan tipe data int 
+
+
+    Tetapkan 3 ke variabel nomor
+
+        
+    Cetak variabel nomor
+*/
+    }
+}
