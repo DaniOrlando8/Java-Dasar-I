707443a209e3e4f4915ec1586de6f53253ff1f7c
Dani Orlando <daniorlando8@gmail.com>
2021-10-29 14:58:53 +0700

    29/10/2021

---
+++ 13 - Variabel dengan tipe data double/src/Soal.java
@@ -0,0 +1,23 @@
+/* ~ Soal ~
+
+    1. Kerjakanlah soal dibawah ini.
+*/
+public class Soal {
+    public static void main(String[] args) {
+        int month = 12;
+        int date = 31;
+        
+        // Gabungkan variable dengan "/" untuk mencetak "12/31"
+        
+        
+        // Cetak hasil dari 7 / 2
+        
+        
+        // Cetak hasil dari 7.0 / 2.0
+        
+        
+        // Cetak hasil dari 7 / 2.0
+        
+               
+    }
+}
