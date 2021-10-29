434e73988aff1ed9bc47c2ac15b82418ac7d887a
Dani Orlando <daniorlando8@gmail.com>
2021-10-29 14:40:03 +0700

    29/10/2021

---
+++ 13 - Variabel dengan tipe data double/src/Main.java
@@ -0,0 +1,19 @@
+/* ~ Variabel & double ~
+
+    Kita sudah belajar menggunakan tipe int untuk bekerja dengan integer. 
+    Namun bagaimana jika kita ingin bekerja dengan angka desimal?
+    Kita dapat menggunakan float dan double! Tetapi, didalam pelajaran ini kita hanya akan menggunakan double saja. 
+    Ada banyak tipe data Java lainnya, silakan melakukan pencarian "Tipe Data Java" di internet jika Anda tertarik!
+    Perhatian: untuk pemrograman, angka desimal dipisahkan dengan titik, bukan koma.
+    Seperti contoh dibawah ini.
+*/
+public class Main {
+    public static void main(String[] args) throws Exception {
+        double number1 = 8.5;
+        double number2 = 3.4;
+
+        System.out.println(number1 + number2);
+        System.out.println(number1 - number2);
+
+    }
+}
